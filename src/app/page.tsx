91af4d1b91268dfadac304798873e1499a57a5f3
DivergenteNM <nicolasmdivergent@gmail.com>
2024-08-11 16:59:09 -0500

    UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,11 +20,11 @@ export default function Home(): JSX.Element {
     T_ambiente: 20,
     T_segura: 35,
     k: 0.1,
-    T_comparacion: 68, //Temperatura de comparación
-    t_comparacion: 2  //Tiempo de comparación
+    T_comparacion: 50, //Temperatura de comparación
+    t_comparacion: 5  //Tiempo de comparación
   });
 
-  
+
 
   // Constantes
   const T_inicial: number = 70;  // Temperatura inicial del servidor en °C
@@ -64,7 +64,7 @@ export default function Home(): JSX.Element {
     //Calculamos primeramente D en base a la temperatura inicial y la temperatura ambiente
     const D: number = inputValues.T_inicial - inputValues.T_ambiente; //recordamos que e ^(-kt) = 1 cuando t = 0
     //Calculamos k en base a la temperatura de comparación
-    const K: number = Math.log((inputValues.T_comparacion - inputValues.T_ambiente)/D)*(-1/inputValues.t_comparacion);
+    const K: number = Math.log((inputValues.T_comparacion - inputValues.T_ambiente) / D) * (-1 / inputValues.t_comparacion);
     if (K !== inputValues.k) {
       setInputValues((prevInputValues) => ({
         ...prevInputValues,
@@ -90,64 +90,98 @@ export default function Home(): JSX.Element {
   };
 
   return (
-    <div className={styles.container}>
-      <h1>Ley de Newton de Enfriamiento - Servidor</h1>
-      <input
-        type="number"
-        id="T_inicial"
-        value={inputValues.T_inicial}
-        onChange={handleSubmit}
-        className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
-        placeholder=" "
-      />
+    <body className={styles.body}>
+      <header className={styles.header}>
+        <div className={styles.headerContent}>
+          <div className={styles.headerTXT}>
+            <h1>
+              Ley de Newton de Enfriamiento - Servidor
+            </h1>
+          </div>
+        </div>
 
-      <input
-        type="number"
-        id="T_ambiente"
-        value={inputValues.T_ambiente}
-        onChange={handleSubmit}
-        className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
-        placeholder=" "
-      />
+      </header>
 
-      <input
-        type="number"
-        id="T_segura"
-        value={inputValues.T_segura}
-        onChange={handleSubmit}
-        className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
-        placeholder=" "
 
-      />
-      <input
-        type="number"
-        id="T_comparacion"
-        value={inputValues.T_comparacion}
-        onChange={handleSubmit}
-        className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
-        placeholder=" "
-      />
+      <div className={styles.container}>
 
-      <input
-        type="number"
-        id="t_comparacion"
-        value={inputValues.t_comparacion}
-        onChange={handleSubmit}
-        className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
-        placeholder=" "
-      />
+        <div className={styles.inputGroup}>
+          <label htmlFor="T_inicial">Temperatura inicial (C°)</label>
+          <input
+            type="number"
+            id="T_inicial"
+            value={inputValues.T_inicial}
+            onChange={handleSubmit}
+            className={styles.inputField}
+            placeholder='Temperatura inicial C°'
+          />
+        </div>
 
-      <h2>Valor aproximado de k es: {inputValues.k}</h2>
-      <CoolingChart {...chartData} />
-      
+        <div className={styles.inputGroup}>
+          <label htmlFor="T_ambiente">Temperatura ambiente (C°)</label>
+          <input
+            type="number"
+            id="T_ambiente"
+            value={inputValues.T_ambiente}
+            onChange={handleSubmit}
+            className={styles.inputField}
+            placeholder='Temperatura ambiente C°'
+          />
+        </div>
 
-      {chartData.tiempoSeguro === undefined ? (
-      <p>El servidor no alcanza la temperatura segura de {inputValues.T_segura}°C.</p>
-    ) : (
-      <p>El servidor alcanza la temperatura segura de {inputValues.T_segura}°C después de {chartData.tiempoSeguro.toFixed(2)} minutos.</p>
-    )}
-      
-      
-    </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="T_segura">Temperatura segura (C°)</label>
+          <input
+            type="number"
+            id="T_segura"
+            value={inputValues.T_segura}
+            onChange={handleSubmit}
+            className={styles.inputField}
+            placeholder='Temperatura segura C°'
+          />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label htmlFor="T_comparacion">Temperatura de comparación (C°)</label>
+          <input
+            type="number"
+            id="T_comparacion"
+            value={inputValues.T_comparacion}
+            onChange={handleSubmit}
+            className={styles.inputField}
+            placeholder='Temperatura de comparación C°'
+          />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label htmlFor="t_comparacion">tiempo de comparación (min)</label>
+          <input
+            type="number"
+            id="t_comparacion"
+            value={inputValues.t_comparacion}
+            onChange={handleSubmit}
+            className={styles.inputField}
+            placeholder='tiempo de comparación (min)'
+          />
+        </div>
+
+        <div className={styles.containerK}>
+          <h2>Valor aproximado de k es: {inputValues.k.toFixed(5)}</h2>
+        </div>
+        <CoolingChart {...chartData} />
+
+        <div className={styles.containerK}>
+          {chartData.tiempoSeguro === undefined ? (
+            <p>El servidor no alcanza la temperatura segura de {inputValues.T_segura}°C.</p>
+          ) : (
+            <p>El servidor alcanza la temperatura segura de {inputValues.T_segura}°C después de {chartData.tiempoSeguro.toFixed(2)} minutos.</p>
+          )}
+        </div>
+
+
+
+      </div>
+    </body>
+
   );
 }
